668bca37bbd8c75e471c902eb4c686f1b2354c7c
nakyeonko3 <badaclock@gmail.com>
2022-12-01 00:56:17 +0900

    test

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 # https://stackoverflow.com/questions/43205568/python-flask-server-with-mqtt-subscription
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import paho.mqtt.client as mqtt
 import mosquitto_mqtt_pi
 import read_csvfile
 from flask_sock import Sock
+import random
 port = 5000
 
 app = Flask(__name__)
 sock = Sock(app)
 port = 5000
 
-sensordata = ""
 
 @app.route('/')
 def render_mainpage():
-    sensor_data = read_csvfile.read_csv_Sensor_value_last()
-    return render_template('index.html', data=sensor_data)
+    return render_template('index.html', name="nakyeonko")
 
-@sock.route('/echo')
-def echo(sock):
-    while True:
-        data = sock.receive()
-        sock.send(data)
+# @sock.route('/getSensorData')
+# def getSensorData():
+#     sensor_data = read_csvfile.read_csv_Sensor_value_last()
+#     return jsonify({'sensor_data':sensor_data})
+
+@app.route('/getSensorData')
+def getSensorData():
+    return jsonify({'sensor_data':random.randint(0, 9)})
 
 if __name__ == '__main__':
     mosquitto_mqtt_pi.mqtt_init()
