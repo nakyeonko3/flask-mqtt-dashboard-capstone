2c18353ce10cbf5222ef49e4cfedc02ea6b5dbed
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2022-12-09 23:28:09 +0900

    update

--- static/js/on_off_button.js
+++ static/js/on_off_button.js
@@ -1,4 +1,5 @@
 const onOffButton = document.getElementById('on-off-button');
+const autoModeElement = document.getElementById('auto-mode');
 
 const turnOn = () => {
   setInnerText('turn off');
@@ -11,8 +12,10 @@ const turnOff = () => {
 };
 
 let toggle = true;
+let handleAutobutton_toggle = true;
 
 const handleClick = async () => {
+  console.log('clicked');
   if (toggle === true) {
     turnOn();
     toggle = false;
@@ -26,8 +29,28 @@ const setInnerText = (text) => {
   onOffButton.innerText = text;
 };
 
+const handleAutobuttonClick = () => {
+  onOffButton.classList.toggle('none');
+  if (handleAutobutton_toggle === true) {
+    if (PHSensorValue >= 7.5 && PHSensorValue <= 8.5) {
+      turnOff();
+    } else if (PHSensorValue < 7.5) {
+      turnOn();
+    } else if (PHSensorValue > 8.5) {
+      turnOn();
+    }
+    handleAutobutton_toggle = false;
+    autoModeElement.innerText = 'auto mode off';
+  } else {
+    turnOff();
+    handleAutobutton_toggle = true;
+    autoModeElement.innerText = 'auto mode on';
+  }
+};
+
 const onOffButtonInit = () => {
   onOffButton.addEventListener('click', handleClick);
+  autoModeElement.addEventListener('click', handleAutobuttonClick);
 };
 
 onOffButtonInit();
--- static/js/ph_warning.js
+++ static/js/ph_warning.js
@@ -2,6 +2,7 @@ const pH_value_Element = document.getElementById('pH_value');
 const ph_warning_Element = document.getElementById('ph_warning');
 
 let PHSensorValue = 0;
+
 const getPHSensorData = () => {
   return fetch('/getPHSensorData')
     .then((response) => response.json())
@@ -16,34 +17,29 @@ const renderPHSensorValue = async () => {
 
 const phWarning = (PHSensorValue) => {
   if (PHSensorValue >= 7.5 && PHSensorValue <= 8.5) {
-    turnOff();
     ph_warning_Element.innerText = '적정 ph';
   } else if (PHSensorValue < 7.5) {
     ph_warning_Element.innerText = '경고 ph가 너무 낮습니다..';
-    turnOn_15s();
   } else if (PHSensorValue > 8.5) {
     ph_warning_Element.innerText = '경고 ph가 너무 높습니다..';
-    turnOn_15s();
   }
 };
 
-let toggle15s = true;
-
-setInterval(() => {
-  toggle15s = true;
-}, 60000);
-
-const turnOn_15s = async () => {
-  if (toggle15s === true) {
-    turnOn();
-    setTimeout(function () {
-      turnOff();
-      toggle15s = false;
-    }, 15000);
-  }
-};
-
-document.getElementById('turnOn15s').addEventListener('click', turnOn_15s);
+// setInterval(() => {
+//   if (PHSensorValue >= 7.5 && PHSensorValue <= 8.5) {
+//     turnOff();
+//     setTimeout(() => {
+//       turnOn();
+//     }, 15000);
+//   } else if (PHSensorValue < 7.5) {
+//     turnOn();
+//     setTimeout(() => {
+//       turnOff();
+//     }, 15000);
+//   } else if (PHSensorValue > 8.5) {
+//     turnOn();
+//   }
+// }, 60000);
 
 const initPh = () => {
   setInterval(renderPHSensorValue, 1000);
